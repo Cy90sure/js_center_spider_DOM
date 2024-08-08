8f6b4980a3375e70eeff670428c74ee58dbfb83e
Maria <maria.pozniak2013@gmail.com>
2024-08-08 15:27:27 +0300

    make build

---
+++ index.d5cb73f8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.top=(t.clientWidth-e.clientHeight)/2+"px",e.style.left=(t.clientWidth-e.clientWidth)/2+"px";
+//# sourceMappingURL=index.d5cb73f8.js.map
